14102c3669eea1f2c81fc0832106a79e95cddfd9
benhardy <benhardy+github@gmail.com>
2010-02-12 15:28:59 -0800

    added readme, small main fixes

--- src/main/scala/net/aethersanctum/fractus/FractusMain.scala
+++ src/main/scala/net/aethersanctum/fractus/FractusMain.scala
@@ -9,18 +9,16 @@ object FractusMain {
     def main(argv:Array[String]) {
         println("args supplied: " + argv.length)
         if (argv.length != 1 && argv.length != 3) {
-            System.err.println("Must supply a ruleset class argument")
+            System.err.println("USAGE: fractus.sh FRACTALNAME [width height]")
             System.exit(-1)
         }
         val rules = new RuleSetFinder().find(argv(0))
 
-        var iwidth = DEFAULT_IMAGE_WIDTH;
-        var iheight = DEFAULT_IMAGE_HEIGHT;
-        if (argv.length == 3) {
-            iwidth = Integer.parseInt(argv(1))
-            iheight = Integer.parseInt(argv(2))
+        val (iwidth, iheight) = if (argv.length == 3) {
+            (Integer parseInt argv(1), Integer parseInt argv(2))
+        } else {
+            (DEFAULT_IMAGE_WIDTH, DEFAULT_IMAGE_HEIGHT)
         }
-
         val mainwin = new Fractus(rules, iwidth, iheight)
         mainwin.pack()
         mainwin setVisible true
